9eb7263e108828f13d970810288b0b4a37a3e181
Ryan Talbot <ryan-talbot@outlook.com>
2022-09-13 01:07:33 +0100

    add main function
    Adding the main function where the core program
    will live.

---
+++ Main.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/env perl
+
+use Modern::Perl '2020';
+use autodie;
+
+exit main ( @ARGV );
+
+sub main {
+
+    print q(
+        _   _  _  __  _ _  _   _   _   __  _ _ 
+        | \_/ || |/ _|| U || \_/ | / \ / _|| U |
+        | \_/ || |\_ \|   || \_/ || o |\_ \|   |
+        |_| |_||_||__/|_n_||_| |_||_n_||__/|_n_|
+                Written by Ryan Talbot
+            https://github.com/RyanTalbot
+
+                mishmash - Version 0.01
+
+    );
+
+    return 0;
+}
